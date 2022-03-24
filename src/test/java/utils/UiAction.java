1179a382ab8220e37900e33bfae407756036b142
scrappy2k20 <zrdrhoades@gmail.com>
2022-03-23 20:10:47 -0400

    worked on for loop

--- src/test/java/utils/UiAction.java
+++ src/test/java/utils/UiAction.java
@@ -4,5 +4,11 @@ public class UiAction {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        for(int num = 0; num <= 10; num++) {
+            System.out.println(num);
+        }
     }
+
+
 }
